08ca20fb066ec18519f3c6f9157a6732f628de5a
Xiaoyun Yang <xiaoyun.ouo@gmail.com>
2018-10-30 09:15:01 -0700

    Set up canvas

--- app/index.js
+++ app/index.js
@@ -8,3 +8,11 @@ import 'styles/index.scss';
 // ================================
 // START YOUR APP HERE
 // ================================
+
+document.addEventListener('DOMContentLoaded', () => {
+  const canvasEl = document.getElementsByTagName('canvas')[0];
+  canvasEl.width = 500;
+  canvasEl.height = 500;
+
+  const context = canvasEl.getContext('2d');
+});
